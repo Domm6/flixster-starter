18b09b73998f4c7fe3923c513f10cc2fa3523800
Dominic Parker <dparker206@icloud.com>
2024-06-12 03:30:17 -0400

    add api + modal open/close

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,22 @@
 import './App.css';
 import Header from './components/Header';
 import MovieCard from './components/MovieCard';
+import MovieList from './components/MovieList';
 import Footer from './components/Footer';
 import Modal from './components/Modal';
 import grownupsImage from './assets/grownups.jpg';
 
 
 function App() {
+  const [isModalVisible, setIsModalVisible] = useState(false);
 
+  const openModal = () => {
+    setIsModalVisible(true);
+  };
+
+  const closeModal = () => {
+    setIsModalVisible(false);
+  };
 
   return (
     <div className="App">
@@ -25,6 +34,7 @@ function App() {
       <div className='movie-modal'>
         <Modal movieTitle="Grown Ups" releaseDate="11/04/2003" overview="A movie about a old grown ups who rekindle and live there lives as kids again."
         genre="Comedy" movieArt={grownupsImage}></Modal>
+        
       </div>
       <div className='app-footer'>
         <Footer></Footer>
--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,21 +1,44 @@
+import React, {useEffect, useState} from 'react';
+import MovieCard from './MovieCard';
 import "./MovieList.css"
 
 function MovieList () {
-      // fetch movie database api
-    const fetchData = async () => {
-    try {
-      const apiKey = import.meta.env.VITE_APP_API_KEY;
-      const response = await fetch(`https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${apiKey}`);
-      if (!response.ok) {
-        throw new Error('Failed to fetch weather data');
-      }
-      const data = await response.json();
-      setWeatherData(data);
-    } catch (error) {
-      console.error(error);
-    }
-  };
+    const [movies, setMovies] = useState([]);
 
+    useEffect(() => {
+        const fetchMovies = async () => {
+            try {
+                const response = await fetch('`https://api.themoviedb.org/3/movie/popular?api_key=${process.env.REACT_APP_TMDB_API_KEY}`');
+                const data = await response.json();
+                setMovies(data.results);
+            } catch(errors) {
+                console.error("Error fetching moives:", errors);
+            }
+        };
+
+        fetchMovies();
+    }, []);
+
+
+
+    // fetch movie database api
+    const fetch = require('node-fetch');
+
+    const url = 'https://api.themoviedb.org/3/movie/now_playing?language=en-US&page=1';
+    const options = {
+    method: 'GET',
+    headers: {
+        accept: 'application/json',
+        Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJkZTdkMDQ1NjQ1MWQ4MjAzN2JkYjViZDZjOTIyYjkxMyIsInN1YiI6IjY2NjdkNjM5NjljZjhmN2ZiMDg1YmY0YiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.IHp2bMxcshTEp8GwPeQxuJsldao0ym5L1KVgBnTdhY4'
+    }
+    };
+
+    fetch(url, options)
+    .then(res => res.json())
+    .then(json => console.log(json))
+    .catch(err => console.error('error:' + err));
+    
+      
     return (
       <div></div>
 
