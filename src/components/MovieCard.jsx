ca773461febdfe076d972776d23cdfe38f8e7e94
Dominic Parker <dparker206@icloud.com>
2024-06-13 22:08:15 -0700

    default images for missing ones

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,9 +1,13 @@
-import "./MovieCard.css"
+import "./MovieCard.css";
 
-function MovieCard (props) {
+const DEFAULT_IMAGE = "https://st4.depositphotos.com/21557188/23286/v/450/depositphotos_232860038-stock-illustration-movie-strip-simple-silhouette-black.jpg";
+
+function MovieCard(props) {
+    const posterUrl = props.movieArt ? `https://image.tmdb.org/t/p/w500/${props.movieArt}` : DEFAULT_IMAGE;
+
     return (
         <div className="movie-card" onClick={(e) => props.openModal(props.id)}>
-            <img src={`https://image.tmdb.org/t/p/w500/${props.movieArt}`} alt="" className="movie-img"/>
+            <img src={posterUrl} alt={`${props.movieTitle} poster`} className="movie-img" />
             <div>
                 <div className="movie-card-text">
                     <h3>{props.movieTitle}</h3>
@@ -12,8 +16,7 @@ function MovieCard (props) {
                 </div>
             </div>
         </div>
-
-    )
+    );
 }
 
-export default MovieCard
+export default MovieCard;
